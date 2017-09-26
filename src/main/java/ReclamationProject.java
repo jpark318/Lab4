e009829c96a92877acd3647b6c9d1c5d54a02330
junbinpark <junbinpark@wirelessprv-10-193-237-174.near.illinois.edu>
2017-09-26 18:07:01 -0500

    Fixed all checkstyle errors

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,3 +1,4 @@
+
 /*
  * The following code needs a lot of TLC. So give it some!
  *
@@ -8,19 +9,57 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * Project for Reclamation.
+ * Program finds out the section in strings that are same in both inputs.
+ */
+public class ReclamationProject {
+
+    /**
+     * @param unused unused input parameter
+     */
+
+    public static void main(final String[] unused) {
+       System.out.println(doit("firefired", "fes"));
+    }
+    /**
+     * @param firstString String
+     * @param secondString String
+     * @return return to r
+     */
+    static String doit(final String firstString, final String secondString) {
+        String result;
+        String firstCopy = firstString;
+        String secondCopy = secondString;
+
+        if (firstCopy.length() > secondCopy.length()) {
+            String temp = firstCopy;
+            firstCopy = secondCopy;
+            secondCopy = temp;
+        }
+        if (firstCopy.equals(secondCopy)) {
+            result = "";
+        } else {
+            result = "";
+        }
         /*
          * For loop with i
+         * Checks out every single substrings possible from
+         * firstString and secondString and compares.
+         * Then it stores if the string is longer than the
+         * string repeated before r where it is result.
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < firstCopy.length(); i++) {
+            for (int j = firstCopy.length() - i; j > 0; j--) {
+                for (int k = 0; k < secondCopy.length() - j; k++) {
+                    if (firstCopy.regionMatches(i, secondCopy, k, j) && j > result.length()) {
+                        result = firstCopy.substring(i,  i + j);
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
